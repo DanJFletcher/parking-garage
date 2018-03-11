8ffe24e9a496041dc7277d6febe2fc8059ab712c
Dan Fletcher <dannyjfletcher@outlook.com>
2018-03-11 16:53:42 -0400

    Set default tickets available to 50

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Redis;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (Redis::get('available_tickets') === null) {
+            Redis::set('available_tickets', '50');
+        }
     }
 
     /**
