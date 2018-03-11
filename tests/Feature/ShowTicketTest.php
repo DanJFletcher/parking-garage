ed4de08c654d61a6a39fa8175785f53e6ad5709b
Dan Fletcher <dannyjfletcher@outlook.com>
2018-03-11 18:00:27 -0400

    Add test for 404

--- tests/Feature/ShowTicketTest.php
+++ tests/Feature/ShowTicketTest.php
@@ -10,6 +10,8 @@ use Illuminate\Support\Facades\Redis;
 
 class ShowTicketTest extends TestCase
 {
+    use RefreshDatabase;
+
     /** @test */
     public function can_show_ticket_if_exists()
     {
@@ -22,5 +24,14 @@ class ShowTicketTest extends TestCase
         $response->assertStatus(200)
             ->assertJsonStructure(['*' => ['ticket_number', 'created_at']])
             ->assertJson(['data' => ['ticket_number' => $ticket->id]]);
+    }
+
+    /** @test */
+    public function see_404_not_found_if_not_exists()
+    {
+        $response = $this->json('get', "api/tickets/1000");
+
+        $response->assertStatus(404)
+            ->assertJson(['status' => '404']);
     }
 }
