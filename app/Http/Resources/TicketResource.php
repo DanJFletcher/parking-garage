f9dabf4d1aef03e8d7ab1ad7d607a738451c5052
Dan Fletcher <dannyjfletcher@outlook.com>
2018-03-11 16:54:57 -0400

    Add available tickets to response

--- app/Http/Resources/TicketResource.php
+++ app/Http/Resources/TicketResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Redis;
 
 class TicketResource extends JsonResource
 {
@@ -17,6 +18,7 @@ class TicketResource extends JsonResource
         return [
             'created_at' => $this->created_at,
             'ticket_number' => $this->id,
+            'available_tickets' => Redis::get('available_tickets'),
         ];
     }
 }
