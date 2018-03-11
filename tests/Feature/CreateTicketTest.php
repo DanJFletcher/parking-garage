90699989eee3b4d6a053dcafb352b36dc1df1f11
Dan Fletcher <dannyjfletcher@outlook.com>
2018-03-11 14:48:19 -0400

    Test that post to /customers returns ticket number

---
+++ tests/Feature/CreateTicketTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CreateTicketTest extends TestCase
+{
+    /** @test */
+    public function creating_customer_returns_ticket_number()
+    {
+        $response = $this->json('post', '/customers');
+
+        $response
+            ->assertStatus(200)
+            ->assertJsonStructure(['id']);
+    }
+}
