bdcc194446bf6e2ea04b7d8eec190654c1445d39
Dan Fletcher <dannyjfletcher@outlook.com>
2018-03-11 20:09:36 -0400

    Test that new payment increments available tickets

--- tests/Feature/CreatePaymentTest.php
+++ tests/Feature/CreatePaymentTest.php
@@ -6,6 +6,7 @@ use App\Ticket;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Redis;
 
 class CreatePaymentTest extends TestCase
 {
@@ -25,5 +26,25 @@ class CreatePaymentTest extends TestCase
         ]);
 
         $response->assertStatus(201);
+    }
+
+    /** @test */
+    public function new_payment_will_add_available_space_to_garage()
+    {
+        Redis::set('available_tickets', 0);
+
+        $ticket = factory(Ticket::class)->create();
+
+        $response = $this->json('post', "api/pay/{$ticket->id}", [
+            'credit_card_number' => '4242424242424242',
+            'credit_card_exp' => '09/22',
+            'credit_card_csv' => '123',
+            'credit_card_name' => 'Jim Bean',
+            'amount' => '600',
+        ]);
+
+        $response->assertStatus(201);
+
+        $this->assertEquals(Redis::get('available_tickets'), '1');
     }
 }
