408f0d6921cb5cb24b127a3661c3f260370d27ee
Dan Fletcher <dannyjfletcher@outlook.com>
2018-03-11 20:56:10 -0400

    Test that starting rate is charged for payment

--- tests/Feature/CreatePaymentTest.php
+++ tests/Feature/CreatePaymentTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Ticket;
+use App\Rate;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -17,13 +18,7 @@ class CreatePaymentTest extends TestCase
     {
         $ticket = factory(Ticket::class)->create();
 
-        $response = $this->json('post', "api/pay/{$ticket->id}", [
-            'credit_card_number' => '4242424242424242',
-            'credit_card_exp' => '09/22',
-            'credit_card_csv' => '123',
-            'credit_card_name' => 'Jim Bean',
-            'amount' => '600',
-        ]);
+        $response = $this->json('post', "api/pay/{$ticket->id}", $this->fakeCreditCardData());
 
         $response->assertStatus(201);
     }
@@ -35,16 +30,32 @@ class CreatePaymentTest extends TestCase
 
         $ticket = factory(Ticket::class)->create();
 
-        $response = $this->json('post', "api/pay/{$ticket->id}", [
-            'credit_card_number' => '4242424242424242',
-            'credit_card_exp' => '09/22',
-            'credit_card_csv' => '123',
-            'credit_card_name' => 'Jim Bean',
-            'amount' => '600',
-        ]);
+        $response = $this->json('post', "api/pay/{$ticket->id}", $this->fakeCreditCardData());
 
         $response->assertStatus(201);
 
         $this->assertEquals(Redis::get('available_tickets'), '1');
+    }
+
+    /** @test */
+    public function ticket_that_is_less_than_one_hour_costs_starting_rate()
+    {
+        $this->withoutExceptionHandling();
+        $ticket = factory(Ticket::class)->create();
+
+        $response = $this->json('post', "api/pay/{$ticket->id}", $this->fakeCreditCardData());
+
+        $response->assertStatus(201)
+            ->assertJson(['data' => ['amount' => Rate::ONE_HOUR]]);
+    }
+
+    private function fakeCreditCardData()
+    {
+        return [
+            'credit_card_number' => '4242424242424242',
+            'credit_card_exp' => '09/22',
+            'credit_card_csv' => '123',
+            'credit_card_name' => 'Jim Bean',
+        ];
     }
 }
