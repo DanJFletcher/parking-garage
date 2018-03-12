e9af535f25b99eb79a15cdb54127888aa44faa01
Dan Fletcher <dannyjfletcher@outlook.com>
2018-03-11 20:09:50 -0400

    Increment available tickets when payment is made

--- app/Http/Controllers/PaymentsController.php
+++ app/Http/Controllers/PaymentsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Payment;
 use App\Http\Resources\PaymentResource;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
 
 class PaymentsController extends Controller
 {
@@ -17,6 +18,8 @@ class PaymentsController extends Controller
     public function store(Request $request)
     {
         $payment = Payment::create($request->all());
+
+        Redis::incr('available_tickets');
 
         return PaymentResource::make($payment);
     }
